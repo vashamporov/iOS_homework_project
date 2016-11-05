d433be863572c6d5c2c1b2982874aebb97725ae0
Admin <admin@Admins-Mac.local>
2016-11-05 14:32:24 +0300

    Initial Commit

---
+++ ios_homework_app/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  ios_homework_app
+//
+//  Created by Admin on 05/11/2016.
+//  Copyright © 2016 vashamporov. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ios_homework_app/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ios_homework_app
+//
+//  Created by Admin on 05/11/2016.
+//  Copyright © 2016 vashamporov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
